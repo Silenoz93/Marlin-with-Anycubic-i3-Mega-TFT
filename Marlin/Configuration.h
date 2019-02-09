2cd40411d30d7e7b7520360d699cb26f68b9c3cd
Silenoz93 <christof.ofner@gmail.com>
2019-02-09 19:39:44 +0100

    Update Configuration.h
    Lower Acceleration, MaxFeedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -619,7 +619,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 6, 60 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 200, 6, 60 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -627,7 +627,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 60, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 1500, 60, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -639,7 +639,7 @@
  */
 #define DEFAULT_ACCELERATION          1200    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
